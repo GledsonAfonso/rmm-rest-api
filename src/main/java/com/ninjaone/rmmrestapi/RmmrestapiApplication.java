d00f14215b04f28f82b6eb91d5135a19d72644ef
Gledson A <g_a6@live.com>
2022-08-11 00:54:45 -0300

    First commit

---
+++ src/main/java/com/ninjaone/rmmrestapi/RmmrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.ninjaone.rmmrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmmrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmmrestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ninjaone/rmmrestapi/RmmrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninjaone.rmmrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmmrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
