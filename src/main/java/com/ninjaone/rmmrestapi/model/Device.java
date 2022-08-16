67535f76d1854193df60eec3450e613d7b722f5b
Gledson A <g_a6@live.com>
2022-08-15 22:18:26 -0300

    Removing services in a cascade when removing a related device

--- src/main/java/com/ninjaone/rmmrestapi/model/Device.java
+++ src/main/java/com/ninjaone/rmmrestapi/model/Device.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -35,7 +36,7 @@ public class Device {
   @JsonProperty("type")
   private DeviceType type;
 
-  @OneToMany(mappedBy = "device", fetch = FetchType.EAGER)
+  @OneToMany(mappedBy = "device", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
   @Column(name = "service_id")
   @JsonProperty("services")
   private List<Service> services = new ArrayList<>();
